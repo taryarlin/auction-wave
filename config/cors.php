777ed7223111f949258e5ce7b1b369e438895f6c
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2024-07-13 00:15:09 +0630

    fix api bug

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+    'allowed_origins' => [env('SANCTUM_STATEFUL_DOMAINS', 'http://localhost:3000', '*')],
 
     'allowed_origins_patterns' => [],
 
