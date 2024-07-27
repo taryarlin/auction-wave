7b4c16669a4bd072de56bfae52446e9a774c0946
Kaung Thant Zin <86509255+KaungThantZin2021@users.noreply.github.com>
2024-07-27 15:11:07 +0630

    [Test] authentication

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ Route::prefix('v1')->group(function () {
     Route::group([
         'middleware' => [
             'auth:sanctum',
-            'verified'
+            // 'verified'
         ]
     ], function () {
         # Logout
