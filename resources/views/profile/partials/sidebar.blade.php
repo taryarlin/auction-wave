fc595c996b49f052f94a0f4a749db1a0fbfbdc0e
Kaung Thant Zin <86509255+KaungThantZin2021@users.noreply.github.com>
2024-08-05 00:10:10 +0630

    [Fix] profile sidebar

---
+++ resources/views/profile/partials/sidebar.blade.php
@@ -0,0 +1,36 @@
+<div class="col-sm-10 col-md-7 col-lg-4">
+    <div class="dashboard-widget mb-30 mb-lg-0">
+        <div class="user">
+            <div class="thumb-area">
+                <div class="thumb border border-secondary">
+                    <img src="{{ $auth_user->profile }}" alt="{{ $auth_user->name }}">
+                </div>
+                <label for="profile-pic" class="profile-pic-edit bg-secondary"><i class="flaticon-pencil"></i></label>
+                <input type="file" id="profile-pic" class="d-none">
+            </div>
+            <div class="content">
+                <h5 class="title"><a href="#0">{{ $auth_user->name }}</a></h5>
+            </div>
+        </div>
+        <ul class="dashboard-menu">
+            <li>
+                <a href="{{ route('profile.dashboard') }}" class="{{ $current_route == request()->routeIs('profile.dashboard') ? 'active' : '' }}"><i class="flaticon-dashboard"></i>Dashboard</a>
+            </li>
+            <li>
+                <a href="{{ route('profile.personal.index') }}" class="{{ $current_route == request()->routeIs('profile.personal.*') ? 'active' : '' }}"><i class="flaticon-user"></i>Personal Profile </a>
+            </li>
+            <li>
+                <a href="mybid.php"><i class="flaticon-auction"></i>My Bids</a>
+            </li>
+            <li>
+                <a href="winning-bid.php"><i class="flaticon-best-seller"></i>Winning Bids</a>
+            </li>
+            <li>
+                <a href="alert.php"><i class="flaticon-alarm"></i>My Alerts</a>
+            </li>
+            <li>
+                <a href="favorite.php"><i class="flaticon-star"></i>My Favorites</a>
+            </li>
+        </ul>
+    </div>
+</div>
--- resources/views/profile/personal/edit.blade.php
+++ resources/views/profile/personal/edit.blade.php
@@ -6,42 +6,7 @@
     <section class="dashboard-section padding-bottom mt--240 mt-lg--325 pos-rel">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-sm-10 col-md-7 col-lg-4">
-                    <div class="dashboard-widget mb-30 mb-lg-0">
-                        <div class="user">
-                            <div class="thumb-area">
-                                <div class="thumb border border-secondary">
-                                    <img src="{{ $auth_user->profile }}" alt="{{ $auth_user->name }}">
-                                </div>
-                                <label for="profile-pic" class="profile-pic-edit bg-secondary"><i class="flaticon-pencil"></i></label>
-                                <input type="file" id="profile-pic" class="d-none">
-                            </div>
-                            <div class="content">
-                                <h5 class="title"><a href="#0">{{ $auth_user->name }}</a></h5>
-                            </div>
-                        </div>
-                        <ul class="dashboard-menu">
-                            <li>
-                                <a href="{{ route('profile.dashboard') }}" class="{{ request()->routeIs('profile.dashboard') ? 'active' : '' }}"><i class="flaticon-dashboard"></i>Dashboard</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('profile.personal.index') }}" class="{{ request()->routeIs('profile.personal.index') . '/*' ? 'active' : '' }}"><i class="flaticon-user"></i>Personal Profile </a>
-                            </li>
-                            <li>
-                                <a href="mybid.php"><i class="flaticon-auction"></i>My Bids</a>
-                            </li>
-                            <li>
-                                <a href="winning-bid.php"><i class="flaticon-best-seller"></i>Winning Bids</a>
-                            </li>
-                            <li>
-                                <a href="alert.php"><i class="flaticon-alarm"></i>My Alerts</a>
-                            </li>
-                            <li>
-                                <a href="favorite.php"><i class="flaticon-star"></i>My Favorites</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                @include('profile.partials.sidebar', ['current_route' => request()->routeIs('profile.personal.*')])
                 <div class="col-lg-8">
                     <div class="row">
                         <div class="col-12">
