9ccaa6b3807c8ff8dc3856231d08483f819f2d1d
Kaung Thant Zin <86509255+KaungThantZin2021@users.noreply.github.com>
2024-08-15 04:02:44 +0630

    [Fix] ui

--- resources/views/frontend/products/_partials/auction_card.blade.php
+++ resources/views/frontend/products/_partials/auction_card.blade.php
@@ -1,7 +1,6 @@
 <div class="auction-item-2" data-aos="zoom-out-up" data-aos-duration="1000">
     <div class="auction-thumb">
         <a href="{{ route('product.detail', $product->id) }}"><img src="{{ $product->acsr_images ? $product->acsr_images[0] : asset('assets/images/no-product-image.png') }}" class="" style="width: 100%; height: 300px; object-fit: cover" alt="product"></a>
-        <a href="#0" class="rating"><i class="far fa-star"></i></a>
         <a href="{{ route('product.detail', $product->id) }}" class="bid"><i class="flaticon-auction"></i></a>
     </div>
     <div class="auction-content">
@@ -15,7 +14,7 @@
                 </div>
                 <div class="amount-content">
                     <div class="current">Current Bid</div>
-                    <div class="amount">{{ $product->starting_price }} MMK</div>
+                    <div class="amount">{{ number_format($product->starting_price) }} MMK</div>
                 </div>
             </div>
             <div class="bid-amount">
@@ -24,7 +23,7 @@
                 </div>
                 <div class="amount-content">
                     <div class="current">Buy Now</div>
-                    <div class="amount">{{ $product->fixed_price }} MMK</div>
+                    <div class="amount">{{ number_format($product->fixed_price) }} MMK</div>
                 </div>
             </div>
         </div>
